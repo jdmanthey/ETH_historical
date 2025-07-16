5b0290bb79162afe81051952ee9b3847d4a83641
Joe Manthey <jdmanthey@gmail.com>
2025-07-16 16:39:21 -0500

    Create 12_mapdamage_rescale.sh

---
+++ 12_mapdamage_rescale.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --chdir=./
+#SBATCH --job-name=mapdamage
+#SBATCH --partition nocona
+#SBATCH --nodes=1 --ntasks=8
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-108
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/04_ethiopia_historical
+
+basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail -n1 )
+
+# define the reference genome
+refgenome=/home/jmanthey/references/Ficedula_albicollis.FicAlb1.5.dna.toplevel.fa
+
+
+source activate mapdamage
+
+module load  gcc/10.1.0 gsl/2.7 r/4.3.0
+
+# make mapdamage output directory
+mkdir ${workdir}/01_bam_files/mapdamage_${basename_array}
+
+# run mapdamage 
+mapDamage -i ${workdir}/01_bam_files/${basename_array}_final.bam -r ${refgenome} \
+-d ${workdir}/01_bam_files/mapdamage_${basename_array} --merge-reference-sequences --rescale \
+--rescale-out=${workdir}/01c_bam_files/${basename_array}_final.bam
+
+# index the final bam file
+samtools index ${workdir}/01c_bam_files/${basename_array}_final.bam
