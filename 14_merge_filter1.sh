e90c27c4c5c154b3487d003214141969264c7a76
Joe Manthey <jdmanthey@gmail.com>
2025-07-17 10:25:35 -0500

    Create 14_merge_filter1.sh

---
+++ 14_merge_filter1.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=merge
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --partition=nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-30
+
+source activate bcftools
+
+# define main working directory
+workdir=/lustre/scratch/jmanthey/04_ethiopia_historical
+
+# define variables
+region_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/scaffolds.txt | tail -n1 )
+
+# run bcftools to merge the vcf files
+bcftools merge -m id --regions ${region_array} ${workdir}/03_vcf/*vcf.gz > ${workdir}/04_vcf/${region_array}.vcf
+
+# max missing = two individuals for heterozygosity; keep invariant
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep cossypha.txt \
+--max-missing 0.85 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/cossypha_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep cristrio.txt \
+--max-missing 0.83 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/cristrio_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep critrist.txt \
+--max-missing 0.87 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/critrist_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep melaenornis.txt \
+--max-missing 0.83 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/melaenornis_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep nectarinia.txt \
+--max-missing 0.85 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/nectarinia_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep sylvia.txt \
+--max-missing 0.80 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/sylvia_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep turdus.txt \
+--max-missing 0.87 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/turdus_het_${region_array}
+
+vcftools --vcf ${workdir}/04_vcf/${region_array}.vcf --keep zosterops.txt \
+--max-missing 0.85 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all \
+--out ${workdir}/06_stats/zosterops_het_${region_array}
+
