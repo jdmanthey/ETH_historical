03b8ac7569fdbd48b9024fdd8f48731387467a7d
Joe Manthey <jdmanthey@gmail.com>
2025-07-25 14:38:54 -0500

    Update 21_snpeff_transversions.sh

--- 21_snpeff_transversions.sh
+++ 21_snpeff_transversions.sh
@@ -7,6 +7,8 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-8
 
+# define main working directory
+workdir=/lustre/scratch/jmanthey/04_ethiopia_historical
 
 # define variables
 input=$( head -n${SLURM_ARRAY_TASK_ID} input_vcf_files.txt | tail -n1 )
