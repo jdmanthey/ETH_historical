3e3625e1d0cec84ef31526e691e4005a8ba5a55b
Joe Manthey <jdmanthey@gmail.com>
2025-07-25 14:28:48 -0500

    Create 21_snpeff_transversions.sh

---
+++ 21_snpeff_transversions.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=snpeff_t
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --partition nocona
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-8
+
+
+# define variables
+input=$( head -n${SLURM_ARRAY_TASK_ID} input_vcf_files.txt | tail -n1 )
+output1=${input%.vcf}.ann.vcf
+output2=${input%.vcf}.ann2.vcf
+
+
+# run snpEff for this chromosome for this species
+java -Xmx16g -jar ~/snpEff/snpEff.jar FicAlb1.5.105 \
+${workdir}/07_transversions/${input} > \
+${workdir}/07_transversions/${output1}
+
+# keep only changes with low, moderate, or high impact
+grep '|LOW|\||MODERATE|\||HIGH|' ${workdir}/07_transversions/${output1} \
+> ${workdir}/07_transversions/${output2}
+
+
+
